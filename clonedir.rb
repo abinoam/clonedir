c794cdfd37ac5d88d86e50bfad63c24f2836973d
Abinoam Praxedes Marques Jr <abinoam@gmail.com>
2015-01-12 22:17:32 -0300

    Grab the args and initialize the paths

--- clonedir.rb
+++ clonedir.rb
@@ -6,3 +6,12 @@ unless ARGV[0] && ARGV[1]
   puts "Usage: #{__FILE__} <src_dir> <dest_dir>"
   exit
 end
+
+src, dest = ARGV
+
+src_path = Pathname.new(src)
+dest_path = Pathname.new(dest)
+
+src_path.find do |path|
+	p path
+end
